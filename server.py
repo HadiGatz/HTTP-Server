a3d1bf881bc63ad3c11e250c07f794e88a2ee40e
Hadi Gatz <albertsviridovski@gmail.com>
2024-11-22 17:49:26 +0200

    [ADD] the server is now extracting the file name, and sends it back to the client

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ PORT = 80
 server = socket.socket()
 server.bind((HOST, PORT))
 
-regex_pattern = re.compile(r"^GET\s+(\/[^\s]*)\s+HTTP\/(1\.[01]|2\.[0-9])$")
+valid_request_regex_pattern = re.compile(r"^GET\s+(\/[^\s]*)\s+HTTP\/(1\.[01]|2\.[0-9])$")
 while True:
     server.listen()
     client_socket, address = server.accept()
@@ -15,8 +15,14 @@ while True:
     packet = client_socket.recv(4096).decode()
     request_line = packet.split('\r\n')[0]
 
-    if (regex_pattern.match(request_line)):
-        print(packet)
+    if (valid_request_regex_pattern.match(request_line)):
+        print("OK")
+        file_name_regex_pattern = re.compile(r"GET\s\/([^\s]*)")
+        file_name_match = file_name_regex_pattern.search(packet)
+        if (file_name_match is not None):
+            file_name = file_name_match.group(1)
+            client_socket.send(file_name.encode())
     else:
+        print("NOT OK")
         client_socket.close()
 
