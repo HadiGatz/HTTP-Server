a349645174b68b9493b5e380baa3b241c665e653
Hadi Gatz <albertsviridovski@gmail.com>
2024-11-22 17:33:20 +0200

    [ADD] added a validation that the HTTP GET request is valid

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 HOST = ''
 PORT = 80
@@ -6,10 +7,16 @@ PORT = 80
 server = socket.socket()
 server.bind((HOST, PORT))
 
+regex_pattern = re.compile(r"^GET\s+(\/[^\s]*)\s+HTTP\/(1\.[01]|2\.[0-9])$")
 while True:
     server.listen()
     client_socket, address = server.accept()
 
     packet = client_socket.recv(4096).decode()
-    print(packet)
+    request_line = packet.split('\r\n')[0]
+
+    if (regex_pattern.match(request_line)):
+        print(packet)
+    else:
+        client_socket.close()
 
