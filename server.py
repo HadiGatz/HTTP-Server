60a776dfdb5b336455586145437e354602185304
Hadi Gatz <albertsviridovski@gmail.com>
2024-11-24 21:43:13 +0200

    [ADD] the server is now sending proper HTTP responses, and supports all Content Types

--- server.py
+++ server.py
@@ -20,15 +20,50 @@ def is_filename_valid(packet):
     return file_name_match
 
 
-def send_requested_file(client_socket, regex_match):
-    file_name = regex_match.group(1)
-    file_path = r"C:\\Users\\User\Documents\\HTTP Server\webroot\webroot\\"+file_name
-    if (os.path.isfile(file_path)):
+def send_requested_file(client_socket, http_response):
+    client_socket.send(http_response)
+
+def get_requested_file_name(packet):
+    file_name_regex_pattern = re.compile(r"GET\s\/([^\s]*)")
+    file_name_match = file_name_regex_pattern.search(packet)
+    if file_name_match:
+        return file_name_match.group(1)
+    return None
+
+def get_file_type(file_path):
+    if (file_path.endswith('.html')):
+        return "Content-Type: text/html; charset=utf-8"
+    elif (file_path.endswith('.jpg')):
+        return "Content-Type: image/jpeg"
+    elif (file_path.endswith('.js')):
+        return "Content-Type: application/javascript; charset=utf-8"
+    elif (file_path.endswith('.css')):
+        return "Content-Type: text/css"
+
+def generate_http_response(file_name):
+    base_path = r"C:\\Users\\User\Documents\\HTTP Server\webroot\webroot\\"
+    file_path = base_path+file_name
+
+    if os.path.isfile(file_path):
+        file_size = os.path.getsize(file_path)
         with open(file_path, "rb") as file_to_send:
             file_data = file_to_send.read()
-            client_socket.send(file_data)
+        
+        
+        response = (
+            f"HTTP/1.0 200 OK\r\n"
+            f"Content-Length: {file_size}\r\n"
+            f"{get_file_type(file_path)}\r\n\r\n"  
+        ).encode() + file_data
     else:
-        client_socket.close()
+        response = (
+            "HTTP/1.0 404 Not Found\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "404 - File Not Found"
+        ).encode()
+    
+    return response
+
 
 while True:
     server.listen()
@@ -36,11 +71,12 @@ while True:
 
     packet = client_socket.recv(4096).decode()
 
-    if (check_request(packet)):
-        print("OK")
-        file_name_match = is_filename_valid(packet)
-        if (file_name_match is not None):
-            send_requested_file(client_socket, file_name_match)
+    if check_request(packet):
+        print("Valid request received.")
+        file_name = get_requested_file_name(packet)
+        if file_name is not None:
+            http_response = generate_http_response(file_name)
+            send_requested_file(client_socket, http_response)
     else:
         print("NOT OK")
         client_socket.close()
